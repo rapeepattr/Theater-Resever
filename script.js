54eec17fbe855cf782c3899d3438be005f12b3f2
Rapeepat Treerattrakul <rapeepat@bananacoding.com>
2025-01-20 15:02:25 +0700

    Add JS

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container')
+const seats = document.querySelectorAll('.row .seat:not(.occupied)')
+
+const count = document.getElementById('count')
+const total = document.getElementById('total')
+
+const selectedMovie = document.getElementById('movie')
+
+let price = +selectedMovie.value // convert to integer
+
+container.addEventListener('click', e => {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected')
+        updateSelected()
+    }
+})
+
+function updateSelected() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected')
+    const countSeats = selectedSeats.length
+
+    count.innerText = countSeats
+    total.innerText = countSeats * price
+}
